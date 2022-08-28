dc3a7df415378db266cd36920112b13855a71a6f
Antonio Dell'Annunziata <contact@antonio-da.dev>
2022-08-28 11:49:37 +0200

    feat: Add dummy request to display image

--- Level1.gd
+++ Level1.gd
@@ -1,9 +1,20 @@
 extends Node
 
+var image_url = "%s/data/images/" % $"/root/Config".base_url
+
 func _ready():
 	$HTTPRequest.connect("request_completed", self, "_images_loaded")
-	$HTTPRequest.request("http://www.mocky.io/v2/5185415ba171ea3a00704eed")
-	pass
+	var error = $HTTPRequest.request("%s/image1.png" % image_url)
+	if error != OK:
+		printerr(error)
 	
 func _images_loaded(result, response_code, headers, body):
-	pass
+	var image = Image.new()
+	var error = image.load_png_from_buffer(body)
+	if error != OK:
+		printerr("Couldn't load the image.")
+
+	var texture = ImageTexture.new()
+	texture.create_from_image(image)
+	
+	$DebugImage.texture = texture
