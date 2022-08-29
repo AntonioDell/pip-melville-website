ee6d4c7841049f216ada314eeb411769b8740f16
Antonio Dell'Annunziata <contact@antonio-da.dev>
2022-08-29 21:20:48 +0200

    fix: Make only level indicators clickable

--- LevelIndicator.gd
+++ LevelIndicator.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends KinematicBody2D
 
 export(NodePath) var player_path_follow_node: NodePath
 export(PackedScene) var scene_to_load: PackedScene
@@ -7,12 +7,14 @@ onready var player_path_follow: PathFollow2D = get_node(player_path_follow_node)
 
 var current_movement: SceneTreeTween
 
-func _input(event):
+
+func _on_Level1Indicator_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton && !current_movement:
 		current_movement = get_tree().create_tween().bind_node(self)
 		current_movement.tween_property(player_path_follow, "unit_offset", 1.0, 5.0)
-		current_movement.connect("finished", self, "movement_finished")
+		current_movement.connect("finished", self, "_movement_finished")
 
-func movement_finished():
+
+func _movement_finished():
 	current_movement = null
 	get_tree().change_scene_to(scene_to_load)
