40d05de2cbec5ff9aaa3a1f959c0a1226ef4fd89
Antonio Dell'Annunziata <contact@antonio-da.dev>
2022-08-29 10:44:56 +0200

    feat: Load gallery from DataRepository

--- Level1.gd
+++ Level1.gd
@@ -1,21 +1,60 @@
 extends Node
 
+const MAX_IMAGE_COUNT := 3
 
-onready var image_url = "%s/data/images/" % $"/root/Config".base_url
+var _image_names := PoolStringArray()
+var _current_position := 0
+
+
+onready var _data_repository := $"/root/DataRepository"
+onready var _image_1 := $Images/Image1
+onready var _image_2 := $Images/Image2
+onready var _image_3 := $Images/Image3
 
 func _ready():
-	$HTTPRequest.connect("request_completed", self, "_images_loaded")
-	var error = $HTTPRequest.request("%s/image1.png" % image_url)
-	if error != OK:
-		printerr(error)
-	
-func _images_loaded(result, response_code, headers, body):
-	var image = Image.new()
-	var error = image.load_png_from_buffer(body)
-	if error != OK:
-		printerr("Couldn't load the image.")
+	if _data_repository.is_data_info_loaded():
+		_init_gallery()
+	else:
+		_data_repository.connect("data_info_loaded", self, "_init_gallery")
 
-	var texture = ImageTexture.new()
-	texture.create_from_image(image)
+func _init_gallery():
+	_image_names = _data_repository.get_image_names()
+	_show_current_images()
+
+
+func _show_current_images():
+	if _current_position >= _image_names.size():
+		_current_position = 0
+	elif _current_position < 0:
+		_current_position = _image_names.size() - _image_names.size() % MAX_IMAGE_COUNT
 	
-	$DebugImage.texture = texture
+	for image_sprite in [_image_1, _image_2, _image_3]:
+		image_sprite.visible = false
+	var current_image_names = Array(_image_names).slice(_current_position, _current_position + (MAX_IMAGE_COUNT - 1))
+	var visible_images := []
+	# TODO: Make dependent on MAX_IMAGE_COUNT
+	if current_image_names.size() == 3:
+		visible_images = [_image_1, _image_2, _image_3]
+	elif current_image_names.size() == 2:
+		visible_images = [_image_1, _image_3]
+	else:
+		visible_images = [_image_2]
+		
+	for i in visible_images.size():
+		var image_sprite := visible_images[i] as Sprite
+		var image_name = current_image_names[i]
+		var image = yield(_data_repository.get_image(image_name), "completed")
+		var texture = ImageTexture.new()
+		texture.create_from_image(image)
+		image_sprite.texture = texture
+		image_sprite.visible = true
+
+
+func _on_BtnNext_pressed():
+	_current_position = _current_position + MAX_IMAGE_COUNT
+	_show_current_images()
+
+
+func _on_BtnPrevious_pressed():
+	_current_position = _current_position - MAX_IMAGE_COUNT
+	_show_current_images()
