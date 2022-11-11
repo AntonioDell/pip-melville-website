612e831d4c47da9d8f8efa9896655d7a81375c1a
Antonio Dell'Annunziata <contact@antonio-da.dev>
2022-11-11 15:03:17 +0100

    feat: Randomize portraits on lever pull

--- levels/gallery/gallery.gd
+++ levels/gallery/gallery.gd
@@ -6,14 +6,20 @@ const TWEEN_DELAY := .5
 var _image_names := PackedStringArray()
 var _current_position := 0
 
-@onready var _portrait_1 := $Images/Portrait1
-@onready var _portrait_2 := $Images/Portrait2
-@onready var _portrait_3 := $Images/Portrait3
+@onready var _portrait_1 := $Portraits/Portrait1
+@onready var _portrait_2 := $Portraits/Portrait2
+@onready var _portrait_3 := $Portraits/Portrait3
+@onready var _portraits_node = $Portraits
+
+var _portraits: Array[Frame] = []
 
 func _ready():
-	for gallery_frame in [_portrait_1, _portrait_2, _portrait_3]:
-		gallery_frame.position.y = - gallery_frame.position.y / 2
-		gallery_frame.visible = false
+	for node in _portraits_node.get_children():
+		if node is Frame:
+			_portraits.append(node)
+	
+	for portrait in _portraits:
+		portrait.visible = false
 	
 	if DataRepository.is_data_info_loaded:
 		_init_gallery()
@@ -30,32 +36,28 @@ func _show_current_images(enter_from_left: bool = true, skip_hide_animation: boo
 	_calculate_current_position()
 	
 	var current_image_names = Array(_image_names).slice(_current_position, _current_position + MAX_IMAGE_COUNT)
-	var visible_images := []
-	# TODO: Make dependent checked MAX_IMAGE_COUNT
-	if current_image_names.size() == 3:
-		visible_images = [_portrait_1, _portrait_2, _portrait_3]
-	elif current_image_names.size() == 2:
-		visible_images = [_portrait_1, _portrait_3]
-	else:
-		visible_images = [_portrait_2]
+	var visible_portraits := _get_visible_portraits(current_image_names.size())
 
-	var old_visible_images := []
-	for gallery_frame in [_portrait_1, _portrait_2, _portrait_3]:
-		if gallery_frame.visible:
-			old_visible_images.append(gallery_frame)
+	var old_visible_portraits := []
+	for portrait in _portraits:
+		if portrait.visible:
+			old_visible_portraits.append(portrait)
+	old_visible_portraits.sort_custom(func(a: Node2D, b: Node2D): 
+		return a.position.x < b.position.x
+	)
 	
 	if not skip_hide_animation:
 		var exit_tween := create_tween().set_parallel()
-		var old_positions := _get_frame_positions(old_visible_images.size())
+		var old_positions := _get_frame_positions(old_visible_portraits.size())
 		var exit_direction := "exit" if enter_from_left else "entry"
-		for i in old_visible_images.size():
+		for i in old_visible_portraits.size():
 			var index := (-i - 1 if enter_from_left else i) as int
-			var frame := old_visible_images[index] as Frame
+			var frame := old_visible_portraits[index] as Frame
 			_tween_frame(exit_tween, frame, old_positions[exit_direction][index], TWEEN_DELAY*i, enter_from_left)
 		await exit_tween.finished
 		
-	for gallery_frame in old_visible_images:
-		gallery_frame.visible = false
+	for portrait in old_visible_portraits:
+		portrait.visible = false
 	
 	var loaded_images = []
 	var image_paths = []
@@ -64,24 +66,36 @@ func _show_current_images(enter_from_left: bool = true, skip_hide_animation: boo
 		loaded_images.append(image)
 		image_paths.append(DataRepository.get_image_path(image_name))
 	
-	var positions := _get_frame_positions(visible_images.size())
+	var positions := _get_frame_positions(visible_portraits.size())
 	var enter_direction := "entry" if enter_from_left else "exit"
-	for i in visible_images.size():
-		var gallery_frame := visible_images[i] as Frame
+	for i in visible_portraits.size():
+		var portrait := visible_portraits[i] as Frame
 		var texture = ImageTexture.create_from_image(loaded_images[i])
-		gallery_frame.place_inside_boundaries(texture)
-		gallery_frame.position = positions[enter_direction][i]
-		gallery_frame.visible = true
-		if gallery_frame.is_connected("clicked",Callable(self,"_on_GalleryFrame_clicked")):
-			gallery_frame.disconnect("clicked",Callable(self,"_on_GalleryFrame_clicked"))
-		gallery_frame.connect("clicked",Callable(self,"_on_GalleryFrame_clicked").bind(image_paths[i]))
+		portrait.place_inside_boundaries(texture)
+		portrait.position = positions[enter_direction][i]
+		portrait.visible = true
+		if portrait.is_connected("clicked",Callable(self,"_on_GalleryFrame_clicked")):
+			portrait.disconnect("clicked",Callable(self,"_on_GalleryFrame_clicked"))
+		portrait.connect("clicked",Callable(self,"_on_GalleryFrame_clicked").bind(image_paths[i]))
 		
 	var entry_tween := create_tween().set_parallel()
-	for i in visible_images.size():
+	for i in visible_portraits.size():
 		var index := (-i - 1 if enter_from_left else i) as int
-		var gallery_frame := visible_images[index] as Frame
-		_tween_frame(entry_tween, gallery_frame, positions["visible"][index], TWEEN_DELAY*i, enter_from_left)
+		var portrait := visible_portraits[index]
+		_tween_frame(entry_tween, portrait, positions["visible"][index], TWEEN_DELAY*i, enter_from_left)
 	await entry_tween.finished
+
+
+func _get_visible_portraits(amount: int) -> Array[Frame]:
+	var _amount: int = amount
+	if _amount > _portraits.size() or _amount > MAX_IMAGE_COUNT:
+		_amount = MAX_IMAGE_COUNT
+	
+	var _possible_portraits := _portraits.duplicate()
+	_possible_portraits.shuffle()
+	
+	var result := _possible_portraits.slice(0, _amount)
+	return result
 
 
 func _get_frame_positions(number_of_frames: int) -> Dictionary:
