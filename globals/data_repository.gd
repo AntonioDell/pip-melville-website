529eed6d7c2cff3bc8142fde75a55f57e418ae80
Antonio Dell'Annunziata <contact@antonio-da.dev>
2022-12-04 08:10:45 +0100

    build: Create test deployment workflow

--- globals/data_repository.gd
+++ globals/data_repository.gd
@@ -4,12 +4,14 @@ extends Node
 signal data_info_loaded
 
 
-const DATA_IDENTIFIER := "data"
-
-
+## Dicctionary containing all meta information on the S3 files.
+## 
+## Example: A file stored as '/some/arbitrary/folders/sample.png' will be contained as
+## {"some": {"arbitrary": {"folders": {"sample.png": {"path": "<url to retrieve this file>", "size": <file size>} }}}}
 var _data_info := {}
-var is_data_info_loaded := false
 
+## If true, it is safe to request data through the provided methods.
+var is_data_info_loaded := false
 
 @onready var base_url = Config.base_url
 
@@ -20,10 +22,11 @@ func _ready():
 
 ## Get all available names of images in the repository.
 func get_image_names() -> Array:
-	if not _data_info.has("images"):
+	# Our AWS bucket is setup with the folder /data/images
+	if not _data_info.has("data") or not _data_info["data"].has("images"):
 		return []
 	var name_list = []
-	for image_name in _data_info["images"]:
+	for image_name in _data_info["data"]["images"]:
 		# On AWS buckets the folders get their own xml entry -> image with empty name gets parsed
 		# This if statement is not necessary with linode object storage
 		if image_name != "":
@@ -33,10 +36,10 @@ func get_image_names() -> Array:
 
 ## Get web path for image name
 func get_image_path(name: String) -> String:
-	if not _data_info["images"].has(name):
+	if not _data_info["data"]["images"].has(name):
 		printerr("DataRepository: No image named %s exists." % name);
 		return ""
-	return _data_info["images"][name]["path"]
+	return _data_info["data"]["images"][name]["path"]
 
 
 ## Get the image with the associated name.
@@ -44,13 +47,13 @@ func get_image_path(name: String) -> String:
 ##
 ## Usage: var image = await DataRepository.get_image(name).completed
 func get_image(name: String):
-	if not _data_info["images"].has(name):
+	if not _data_info["data"]["images"].has(name):
 		printerr("DataRepository: No image named %s exists." % name);
 		return null
 	
-	var error = $HTTPRequest.request(_data_info["images"][name]["path"])
+	var error = $HTTPRequest.request(_data_info["data"]["images"][name]["path"])
 	var response = await $HTTPRequest.request_completed 
-	if error != OK or response[0] != HTTPRequest.RESULT_SUCCESS:
+	if error != OK:
 		return null 
 	
 	# request_completed emits result, response_code, headers and body
@@ -58,6 +61,7 @@ func get_image(name: String):
 	var body = response[3]
 	var image = Image.new()
 	var image_type = name.split(".")[-1]
+	# For now we only accept png, jpg and bmp images
 	match image_type:
 		"png": error = image.load_png_from_buffer(body)
 		"jpg", "jpeg": error = image.load_jpg_from_buffer(body)
@@ -70,18 +74,25 @@ func get_image(name: String):
 	return image
 
 
+## Internal method.
+##
+## Reload the necessary informations to load data from S3 bucket
 func _reload_data_info():
 	print("DataRepository: Reload data info...")
 	is_data_info_loaded = false
+	# A request to the S3 base_url and 'Accept: applicaiton/xml' will return a list of all resources
+	# inside the S3 bucket in XML format
 	var error = $HTTPRequest.request(base_url, ["Accept: application/xml"])
 	if error != OK:
 		printerr(error)
 		return
 	var result = await $HTTPRequest.request_completed
+	# The result XML needs to be parsed node by node
 	var parser := XMLParser.new()
 	var body = result[3]
 	parser.open_buffer(body)
 	while parser.read() == OK:
+		# We only care about <Contents> node, since this contains information on all stored files
 		if(parser.get_node_type() == XMLParser.NODE_ELEMENT && parser.get_node_name() == "Contents"):
 			_parse_contents(parser)
 	print("DataRepository: Reload finished.")
@@ -89,6 +100,9 @@ func _reload_data_info():
 	emit_signal("data_info_loaded")
 
 
+## Internal method.
+## 
+## Parse the <Contents> node and extract information about all files stored inside the S3 bucket.
 func _parse_contents(parser: XMLParser):
 	var end_tags_to_ignore := 0
 	var ignore_end_tag := false
