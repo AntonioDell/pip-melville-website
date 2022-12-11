52d800cd64a1685c35c77d7489653a032bcc17ec
Antonio Dell'Annunziata <contact@antonio-da.dev>
2022-12-11 11:05:17 +0100

    feat: Add path from start to hut indicator

--- levels/map/level_indicator.gd
+++ levels/map/level_indicator.gd
@@ -1,14 +1,21 @@
+@tool
 class_name LevelIndicator
 extends CharacterBody2D
 
 signal clicked(level_indicator)
 
 @export var scene_to_load: PackedScene
-@export var map_position: int
+@export var map_position: int = -1
 @export var is_click_disabled := true:
 	set(value):
 		is_click_disabled = value
 		$CollisionShape2D.disabled = is_click_disabled
+
+
+func _ready():
+	if map_position == -1:
+		var count = get_tree().get_nodes_in_group("level_indicators").size()
+		map_position = count-1
 
 
 func _on_LevelIndicator_input_event(viewport, event, shape_idx):
